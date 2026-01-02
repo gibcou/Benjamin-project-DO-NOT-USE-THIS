3d3ee8118caf8b7fdc7c21e2facf7b24fee8a71b
gibcou <raycoutley646@gmail.com>
2026-01-02 14:26:06 -0700

    Fix asset import path; build for GitHub Pages and add 404

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Benjamin-project-DO-NOT-USE-THIS/",
+  build: {
+    outDir: "docs",
+  },
 });
