d1402b6aa8a610030f7588a17e0dd1c433a870ab
gibcou <raycoutley646@gmail.com>
2026-01-02 14:30:20 -0700

    Switch to HashRouter for GitHub Pages reliability

--- main.jsx
+++ main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Benjamin-project-DO-NOT-USE-THIS">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
